39ced70b895d72333ec9d8921b8e7f4c49d781e5
WongYC-66 <yengchien@hotmail.com>
2025-06-08 22:42:00 +0800

    update: /bot  help

--- netlify/handleBotEvent.js
+++ netlify/handleBotEvent.js
@@ -195,19 +195,20 @@ export const handleBotEvent = async (rawBody) => {
 
     if (subCommand === 'help') {
         const helpString =
-            `VNHOES BOT HELP:
+        ```
+            VNHOES BOT HELP:
             /bot equip xxxx         : search and return 1st equip from unofficial library
             /bot monster xxxx       : search and return 1st monster from unofficial library
             /bot item xxxx          : search and return 1st item from unofficial library
             /bot help               : show help
-        `
+        ```
 
         return {
             statusCode: 200,
             body: JSON.stringify({
                 type: 4,
                 data: {
-                    contents: codeBlock(helpString)
+                    contents: helpString
                 },
             }),
             headers: { 'Content-Type': 'application/json' },
