b33468de1a61b2c6dc2a2de20e54cd3e1ce696cd
WongYC-66 <yengchien@hotmail.com>
2025-06-12 22:21:48 +0800

    update /troll pat

--- netlify/troll.js
+++ netlify/troll.js
@@ -72,7 +72,7 @@ export const getTrollSackPavResponse = async () => {
 export const getTrollPatResponse = async (triggeredUser, targetUser, event) => {
     const isLocalTestServer = event.headers.host.includes('localhost')
     console.log(triggeredUser.id, targetUser, { isLocalTestServer })
-    const interaction = event.body
+    const interaction = JSON.parse(event).body
     console.log(interaction)
 
     const baseImageUrl = 'https://media1.tenor.com/m/Wc_Sv1zFlmQAAAAC/nix-voltare-fsp-nix-voltare-fsp-en.gif';      // Pat Image URL
